603fd6388ae0d2c0d4c279491a87f6029f446fff
CSaraniti <CarloSaraniti3@gmail.com>
2019-07-10 11:19:45 -0500

    Added User Location

--- Pizza Hunter/ViewController.swift
+++ Pizza Hunter/ViewController.swift
@@ -11,9 +11,14 @@ import MapKit
 import CoreLocation
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var mapView: MKMapView!
+    
+    let locationManager = CLLocationManager()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+            locationManager.requestWhenInUseAuthorization()
+            mapView.showsUserLocation = true
     }
 
 
